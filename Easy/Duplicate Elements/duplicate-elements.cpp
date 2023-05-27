017071a4b23950c6c97bab878adfe6a0dc992c4d
KshitizTyagi1011 <108491856+KshitizTyagi1011@users.noreply.github.com>
2023-05-27 18:57:53 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Duplicate Elements/duplicate-elements.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+ 
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void SortedDuplicates(int n, vector<int> &arr) {
+        set<int>ans;
+        sort(arr.begin(), arr.end());
+        for(int i=0; i<n-1; i++){
+            if(arr[i] == arr[i+1]) ans.insert(arr[i]);
+        }
+        if(ans.empty()) ans.insert(-1);
+        for(auto i:ans){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n; 
+        scanf("%d",&n);
+        
+        
+        vector<int> arr(n);
+        Array::input(arr,n);
+        
+        Solution obj;
+        
+        obj.SortedDuplicates(n, arr);
+        
+    }
+}
+
+// } Driver Code Ends
