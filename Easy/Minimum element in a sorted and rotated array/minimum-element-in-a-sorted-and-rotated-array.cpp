e74977b2b53d5e0b946db37722aaaf56da24a163
KshitizTyagi1011 <108491856+KshitizTyagi1011@users.noreply.github.com>
2023-08-14 18:21:50 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimum element in a sorted and rotated array/minimum-element-in-a-sorted-and-rotated-array.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int findMin(int arr[], int n){
+        //Find the pivot
+        
+        int s = 0;
+        int e = n-1;
+        int mid = s+(e-s)/2;
+        while(arr[s] > arr[e]){
+            if(arr[mid] > arr[e]) s = mid+1;
+            else e = mid;
+            mid = s+(e-s)/2;
+        }
+        return arr[s];
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int i,a[n];
+		for(i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		Solution ob;
+		cout<<ob.findMin(a, n)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
