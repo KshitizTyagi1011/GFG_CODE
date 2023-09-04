e7ccdc1a2003402f03f4f0429e307d6f68845a34
KshitizTyagi1011 <108491856+KshitizTyagi1011@users.noreply.github.com>
2023-09-04 23:31:09 +0530

    Updated Solution - GfG to GitHub

--- Easy/Search in a Rotated Array/search-in-a-rotated-array.cpp
+++ Easy/Search in a Rotated Array/search-in-a-rotated-array.cpp
@@ -4,21 +4,20 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution{
-    int binary_search(int arr[], int s, int e, int k){
-        int mid = s+(e-s)/2;
-        while(s<=e){
-            if(arr[mid] == k) return mid;
-            else if(arr[mid] < k) s = mid+1;
-            else e = mid-1;
-            mid = s+(e-s)/2;
-        }
+    int Binary_search(int arr[], int s, int e, int key){
+          int mid = s+(e-s)/2;
+          while(s<=e){
+              if(arr[mid] == key) return mid;
+              else if(arr[mid] < key) s = mid + 1;
+              else e = mid - 1;
+              mid = s+(e-s)/2;
+          }
         return -1;
     }
-    
     int get_pivot(int arr[], int s, int e){
         int mid = s+(e-s)/2;
-        while(s<e){
-            if(arr[mid] >= arr[0]) s = mid+1;
+        while(s < e){
+            if(arr[mid] >= arr[0]) s = mid + 1;
             else e = mid;
             
             mid = s+(e-s)/2;
@@ -32,14 +31,12 @@ class Solution{
         
         //complete the function here
         int pivot = get_pivot(A,l,h);
-        //int n = A.size();
-        if((key >= A[pivot]) && (key <= A[h])){
-            return binary_search(A,pivot,h,key);
+        if(key >= A[pivot] && key <= A[h]){    //pivot or last index ke beech mai
+            return Binary_search(A,pivot,h,key);
         }
         else{
-             return binary_search(A,l,pivot-1,key);
+            return Binary_search(A,l,pivot-1,key);  //start or pivot ke beech mai
         }
-        
     }
 };
 
